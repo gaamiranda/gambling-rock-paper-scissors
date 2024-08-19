0c0b89b104e89973af2ec2ac86e03649d10c9be7
gaamiranda <goncalo.andre.miranda@gmail.com>
2024-08-19 16:35:45 +0100

    html css inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+let yourScore = 0
+let computerScore = 0
+
+
+for (let i = 1; i < 3; i++) {
+	yourScore++
+	computerScore++
+	document.getElementById("your_score").textContent = yourScore
+	document.getElementById("computer_score").textContent = computerScore
+}
