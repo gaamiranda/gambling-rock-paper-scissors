787080b26cb26b188e2d5e7ce914332a588c6def
gaamiranda <goncalo.andre.miranda@gmail.com>
2024-08-22 19:40:28 +0100

    new login

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-document.getElementById('login-button').addEventListener('click', function(e) {
+
+document.getElementById('login-form').addEventListener('submit', function(e) {
 	e.preventDefault()
-	let name = document.getElementById("login-name").value
+	let name = document.getElementById("login-username").value
 	let password = document.getElementById("login-password").value
 	if (name === "admin" && password === "admin") window.location.href = "main.html"
 	else alert("Invalid username or password!")
